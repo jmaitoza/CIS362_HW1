5d9403e1f408fc526ed416b813eade2353944ee4
Joshua Maitoza <60358495+jmaitoza@users.noreply.github.com>
2023-03-21 08:57:37 -0400

    started making graphs

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+# Problem 1
+# gonna use columns: "num_shares" and "num_likes"
+# scatter plot, bar chart, histogram
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# create a python program that creates a scatter plot for one column from csv file in /datasets/
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+dataset = pd.read_csv("datasets/Facebook_LiveSellers_Thailand.csv")
+dataset.head()
+# scatter plot of column num_shares and num_likes
+plt.scatter(dataset["num_loves"], dataset["num_likes"])
+# make plot look nicer and less squished together
+plt.xlim(-10, 600)
+# plt.ylim(262, 1000)
+# label the plot
+plt.xlabel("Number of Loves")
+plt.ylabel("Number of Likes")
+# show plot
+plt.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# # create a python program that creates a bar chart for one column from csv file in /datasets/
+# # import numpy as np
+# # import pandas as pd
+# # import matplotlib.pyplot as plt
+#
+dataset = pd.read_csv("datasets/Facebook_LiveSellers_Thailand.csv")
+dataset.head()
+# bar chart of column num_shares and num_likes
+plt.bar(dataset["num_loves"], dataset["num_likes"])
+# make plot look nicer and less squished together
+plt.xlim(-10, 550)
+plt.ylim(0, 2700)
+# label the plot
+plt.xlabel("Number of Loves")
+plt.ylabel("Number of Likes")
 
+# change the x axis to increase by 50
+plt.xticks(np.arange(0, 600, 50))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# show plot
+plt.show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# histogram of num_loves and num_likes
